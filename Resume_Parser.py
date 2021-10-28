dafba10bd8cd17e782ba764ebb9844760dfba43a
MADPROJECT-ar <madprojectar@gmail.com>
2021-10-29 01:21:01 +0530

    version 0.7

--- Resume_Parser.py
+++ Resume_Parser.py
@@ -1,5 +1,4 @@
 import docx2txt as dct
-from textblob import TextBlob
 badWords = ["getter", "synergy", "hard-worker", "hard-working", "thinker", "negotiable", "detail-oriented", "hardworker", "hardworking", "proactive", "thinker", "objective"]
 
 def resumeAnalyzer(resumeFile):
@@ -7,5 +6,4 @@ def resumeAnalyzer(resumeFile):
     word_set = set(badWords)
     phrase_set = set(resume.lower().split())
     resumeWordCorrections = word_set.intersection(phrase_set)
-    correctedResume = TextBlob(resume.lower()).correct()
-    return resumeWordCorrections,correctedResume
+    return resumeWordCorrections
--- views.py
+++ views.py
@@ -5,9 +5,14 @@ import csv
 import os
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
+import Resume_Parser
 
 from flask import Flask, render_template, url_for, request, redirect, Blueprint
 from datetime import datetime
+
+import AudioVideoSplitter
+import Speech_To_Text
+import Speech_Parser
 
 views = Blueprint(__name__, "views")
 
@@ -21,6 +26,21 @@ def resumeanalyzer_page():
     return render_template("resumeanalyzer.html")
 
 @views.route('/uploader/', methods=['POST', 'GET'])
+def analyzeResume():
+    if request.method == 'POST':
+        f2 = request.files['resume_file']
+        f2.save(secure_filename(f2.filename))
+    
+        name2 = "asdf"
+
+        n2 = str(f2)
+        n2 = n2[15:-16]
+        print(n2)
+
+        Resume_Parser.resumeAnalyzer(n2)
+
+
+@views.route('/uploader/', methods=['POST', 'GET'])
 def analyze():
  
     if request.method == 'POST':
@@ -29,11 +49,17 @@ def analyze():
 
         name = "asdf"
 
-        #n = str(f)
-        #n = n[15:-16]
-        #print(n)
+        n = str(f)
+        n = n[15:-16]
+        print(n)
 
         location_videofile =  "D:\MiniProject\AI_Interview_Assistant\Abhishek.mp4"
+
+        AudioVideoSplitter.splitAudio(name,location_videofile)
+        
+        userSpeech = Speech_To_Text.speechToText('\\audio\\'+name+'.wav')
+
+        speechFeedback = Speech_Parser(userSpeech)
 
         face_detector = FER(mtcnn=True)
         input_video = Video(location_videofile)
@@ -62,39 +88,3 @@ def analyze():
         score_comparisons = pd.DataFrame(emotions, columns = ['Human Emotions'])
         score_comparisons['Emotion Value from the Video'] = emotions_values
         score_comparisons.to_csv(name+'.csv', index=False)
-
-
-# @views.route('/emorec', methods=['POST', 'GET'])
-# def emorec():
-#     location_videofile = videoFile
-
-#     face_detector = FER(mtcnn=True)
-#     input_video = Video(location_videofile)
-
-#     processing_data = input_video.analyze(face_detector, display=False)
-
-#     vid_df = input_video.to_pandas(processing_data)
-#     vid_df = input_video.get_first_face(vid_df)
-#     vid_df = input_video.get_emotions(vid_df)
-
-#     pltfig = vid_df.plot(figsize=(20, 8), fontsize=16).get_figure()
-#     pltfig.savefig('output\\'+name+'.png')
-
-#     angry = sum(vid_df.angry)
-#     disgust = sum(vid_df.disgust)
-#     fear = sum(vid_df.fear)
-#     happy = sum(vid_df.happy)
-#     sad = sum(vid_df.sad)
-#     surprise = sum(vid_df.surprise)
-#     neutral = sum(vid_df.neutral)
-
-#     emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-#     emotions_values = [angry, disgust, fear, happy, sad, surprise, neutral]
-
-
-#     score_comparisons = pd.DataFrame(emotions, columns = ['Human Emotions'])
-#     score_comparisons['Emotion Value from the Video'] = emotions_values
-#     score_comparisons.to_csv( name +'.csv', index=False)
-
-# if __name__ == "__main__":
-#     views.run(debug=True,port=5000)
